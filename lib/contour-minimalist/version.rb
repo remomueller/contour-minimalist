500e31e57b1624a4cae597fff35b1b0b1ab03120
Remo Mueller <remosm@gmail.com>
2012-02-13 12:00:30 -0500

    Version bump to 0.1.0.pre

--- lib/contour-minimalist/version.rb
+++ lib/contour-minimalist/version.rb
@@ -2,9 +2,9 @@ module Contour
   module Minimalist
     module VERSION
       MAJOR = 0
-      MINOR = 0
+      MINOR = 1
       TINY = 0
-      BUILD = nil # nil, "pre", "rc", "rc2"
+      BUILD = "pre" # nil, "pre", "rc", "rc2"
 
       STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
     end
