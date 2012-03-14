dacf00683a72e56603d68f12980e41e6d1cb0e2d
Remo Mueller <remosm@gmail.com>
2012-03-14 11:58:57 -0400

    St. Patrick's Day Theme added

--- config/initializers/contour-minimalist.rb
+++ config/initializers/contour-minimalist.rb
@@ -7,6 +7,12 @@ Contour.setup do |config|
   config.month_day = {
     "2-14" => { body_background_color: "#fff2fd",
                 body_background_image: "contour-minimalist/background-valentine.jpg",
-                link_color: "#d93f77" }
+                link_color: "#d93f77" },
+    "3-16" => { body_background_color: "#f0ffe3",
+                body_background_image: "contour-minimalist/background-stpatricks.jpg",
+                link_color: "#ffa200" },
+    "3-17" => { body_background_color: "#f0ffe3",
+                body_background_image: "contour-minimalist/background-stpatricks.jpg",
+                link_color: "#ffa200" }
   }
 end
